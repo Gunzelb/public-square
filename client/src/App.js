a270a1c8d0f40e26f302a3c1f4465241eed25dda
Stephanie Cabrera <stephanie.cabrera.333@gmail.com>
2021-10-14 16:43:24 -0700

    New React app with Chakra template included

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+//Library imports
+import React from 'react';
+import { ChakraProvider, Box, VStack, Grid, theme,} from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+
+// Components
+// import LoginForm from './components/LoginForm';
+// import SignupForm from './components/SignupForm';
+import Post from './components/Post';
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <VStack spacing={8}>
+            {/* <LoginForm /> */}
+            {/* <SignupForm /> */}
+            <Post/>
+          </VStack>
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default App;
