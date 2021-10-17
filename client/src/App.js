c2b523a6cd7154d9827a16238f11473b3e77d0eb
Stephanie Cabrera <stephanie.cabrera.333@gmail.com>
2021-10-16 19:16:37 -0700

    Set to only display the login form

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
 //Library imports
 import React from 'react';
-import './App.css';
 import { ChakraProvider, Box, VStack, Grid, theme,} from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 
 // Components
-// import LoginForm from './components/LoginForm';
+import LoginForm from './components/LoginForm';
 // import SignupForm from './components/SignupForm';
-import Post from './components/Post';
+// import Posts from './components/Posts';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
+        <Grid minH="100vh">
           <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            {/* <LoginForm /> */}
+            <LoginForm />
             {/* <SignupForm /> */}
-            <Post/>
-          </VStack>
+            {/* <Posts/> */}
         </Grid>
       </Box>
     </ChakraProvider>
