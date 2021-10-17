7547362a10203e6dd25e7c8db121ecd1f1df8e20
Stephanie Cabrera <stephanie.cabrera.333@gmail.com>
2021-10-16 19:14:45 -0700

    Used the useState hook for the input fields, handleChange and handleSubmit functions, plogs the data

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,27 +1,77 @@
-import React from "react";
+//Imports
+import React from "react"
+import {useState} from "react"
+
+//Chakra Components
 import { Flex, Input, Heading, Button } from "@chakra-ui/react";
-// import { Box } from "@chakra-ui/react";
 import { FormControl, FormLabel } from "@chakra-ui/react";
 
 
 function LoginForm() {
-    // const [inputUsername, setUserName] = useState("");
-    // const [inputPassword, setPassword] = useState("");
+    const [username, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleInputChange = (e) => {
+        // Getting the value and name of the input which triggered the change
+        const inputType = e.target.name;
+        const inputValue = e.target.value;
+    
+        // Based on the input type, we set the state of either email, username, and password
+        if (inputType === 'username') {
+          setUserName(inputValue);
+        }else {
+          setPassword(inputValue);
+        }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        const login= {
+            username: username,
+            password: password
+        }
+        console.log(login);
+    }
+
 
     return (
         <Flex direction="column" border="1px" p={12} rounded={6}>
             <Heading mb={6}>Log in</Heading>
             <FormControl id="email" isRequired>
+                
                 <FormLabel>Email address</FormLabel>
-                <Input type="email" placeholder="janedoe@email.com" variant="filled" mb={3} />
+                <Input 
+                    type="username" 
+                    name="username" 
+                    placeholder="jane_doe" 
+                    variant="filled" 
+                    mb={3} 
+                    value={username} 
+                    onChange={handleInputChange}
+                />
+
             </FormControl>
+            
             <FormControl id="password" isRequired>
+
                 <FormLabel>Password</FormLabel>
-                <Input type="password" placeholder="********" variant="filled" mb={3} />
+                <Input 
+                    type="password" 
+                    name="password" 
+                    placeholder="********" 
+                    variant="filled" 
+                    mb={3} 
+                    value={password} 
+                    onChange={handleInputChange}
+                />
+
             </FormControl>
-            <Button colorScheme="cyan" variant="outline">
+            
+            <Button colorScheme="cyan" variant="outline" onClick={handleSubmit}>
                 Submit
             </Button>
+        
         </Flex>
     )
 };
