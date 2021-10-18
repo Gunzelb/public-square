23a06a4e4e08ac6647296c43b2866415dc26644a
Stephanie Cabrera <stephanie.cabrera.333@gmail.com>
2021-10-17 22:24:35 -0700

    Added a toast for when the user submits the Signup Form

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,10 +1,19 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
+
+//Chakra
 import { Flex, Input, Heading, Button } from "@chakra-ui/react";
 import { FormControl, FormLabel } from "@chakra-ui/react";
+// import ToastExample from "./themedComponents/Toast";
+import { useToast } from "@chakra-ui/react";
+
+
 
 
 function SignupForm() {
-    
+
+    const toast = useToast();
+
+    //Form states
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [email, setEmail] = useState("");
@@ -12,27 +21,43 @@ function SignupForm() {
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
 
+    //Toast state
+    const [status, setStatus] = useState(false);
+
+    useEffect(() => {
+        if (status) {
+            toast({
+                title: "It Works!",
+                description: "Omg it really did it",
+                status: 'success',
+                duration: 5000,
+                isClosable: true
+            });
+            setStatus(false)
+        }
+    }, [status, toast]);
+
     const handleInputChange = (e) => {
         // Getting the value and name of the input which triggered the change
         const inputType = e.target.name;
         const inputValue = e.target.value;
-    
+
         // Based on the input type, we set the state of either email, username, and password
-        if(inputType === 'firstName'){
+        if (inputType === 'firstName') {
             setFirstName(inputValue);
         }
-        else if(inputType === 'lastName'){
+        else if (inputType === 'lastName') {
             setLastName(inputValue);
         }
-        else if (inputType === "email"){
+        else if (inputType === "email") {
             setEmail(inputValue);
         }
         else if (inputType === 'username') {
             setUsername(inputValue);
-        }else if(inputType === "password") {
+        } else if (inputType === "password") {
             setPassword(inputValue);
         }
-        else{
+        else {
             setConfirmPassword(inputValue);
         }
 
@@ -40,6 +65,11 @@ function SignupForm() {
 
     const handleSubmit = (e) => {
         e.preventDefault()
+
+
+        //Check if the input is empty
+        if(!firstName || !lastName || !email || !username || !password || !confirmPassword){return}
+
 
         const Signup = {
             first_name: firstName,
@@ -50,59 +80,70 @@ function SignupForm() {
             confirm: confirmPassword
         }
 
-
         console.log(Signup);
-        if(password !== confirmPassword){
+        if (password !== confirmPassword) {
             console.log("Passwords do not match!");
+            return
+        }
+        else {
+            setStatus(true)
+
+            //Set the states to empty strings
+            setFirstName("")
+            setLastName("")
+            setEmail("")
+            setUsername("")
+            setPassword("")
+            setConfirmPassword("")
+            return
         }
 
-        //Set the states to empty strings
-        setFirstName("")
-        setLastName("")
-        setEmail("")
-        setUsername("")
-        setPassword("")
-        setConfirmPassword("")
+
+
+
+        // setTimeout(1000, setStatus(false))
 
     }
 
     return (
-        <Flex direction="column" border="1px" p={10} rounded={10} justifySelf="center" 
+        <Flex direction="column" border="1px" p={10} rounded={10} justifySelf="center"
             alignSelf="start" width="45%">
+            {/* <form onSubmit={handleSubmit}> */}
             <Heading mb={4}>Sign up</Heading>
             <Flex direction="row">
                 <FormControl id="firstname" mr={4} isRequired>
                     <FormLabel>First Name</FormLabel>
-                    <Input type="text"  placeholder="Jane" variant="filled" mb={3} 
-                    name="firstName" value={firstName} onChange={handleInputChange} />
+                    <Input type="text" placeholder="Jane" variant="filled" mb={3}
+                        name="firstName" value={firstName} onChange={handleInputChange} />
                 </FormControl>
                 <FormControl id="lastname" isRequired>
                     <FormLabel>Last Name</FormLabel>
-                    <Input type="text"  placeholder="Doe" variant="filled" mb={3} 
-                    name="lastName" value={lastName} onChange={handleInputChange} />
+                    <Input type="text" placeholder="Doe" variant="filled" mb={3}
+                        name="lastName" value={lastName} onChange={handleInputChange} />
                 </FormControl>
             </Flex>
             <FormControl id="username" isRequired>
                 <FormLabel>Username</FormLabel>
-                <Input type="text" placeholder="jane_doe" variant="filled" mb={3} 
-                name="username" value={username} onChange={handleInputChange} />
+                <Input type="text" placeholder="jane_doe" variant="filled" mb={3} autoComplete="username"
+                    name="username" value={username} onChange={handleInputChange} />
             </FormControl>
             <FormControl id="email" isRequired>
                 <FormLabel>Email address</FormLabel>
-                <Input type="email" placeholder="janedoe@email.com" variant="filled" mb={3} 
-                name="email" value={email} onChange={handleInputChange} />
+                <Input type="email" placeholder="janedoe@email.com" variant="filled" mb={3}
+                    name="email" value={email} onChange={handleInputChange} />
             </FormControl>
             <FormControl id="password" isRequired>
                 <FormLabel>Password</FormLabel>
-                <Input type="password" placeholder="********" variant="filled" mb={3} 
-                name="password" onChange={handleInputChange} />
+                <Input type="password" placeholder="********" variant="filled" mb={3} autoComplete="new-password"
+                    name="password" onChange={handleInputChange} value={password} />
             </FormControl>
             <FormControl id="confirmPassword" isRequired>
                 <FormLabel>Confirm password</FormLabel>
-                <Input type="password" placeholder="********" variant="filled" mb={3} 
-                name="confirmPassword" onChange={handleInputChange} />
+                <Input type="password" placeholder="********" variant="filled" mb={3} autoComplete="new-password"
+                    name="confirmPassword" onChange={handleInputChange} value={confirmPassword} />
             </FormControl>
-            <Button colorScheme="cyan" variant="outline" onClick={handleSubmit}>
+            {/* </form> */}
+            <Button colorScheme="cyan" variant="outline" onClick={handleSubmit} type="submit">
                 Submit
             </Button>
         </Flex>
