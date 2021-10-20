c48f348f0248c127db7b3e9b1e968b1004a935e5
Stephanie Cabrera <stephanie.cabrera.333@gmail.com>
2021-10-20 14:33:04 -0700

    Applying React Router to the App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,34 @@
 //Library imports
 import React from 'react';
 import { ChakraProvider, theme, Grid} from '@chakra-ui/react'; //VStack, Box, Grid,
-// import { ColorModeSwitcher } from './ColorModeSwitcher';
 
 // Components
 // import LoginForm from './components/LoginForm';
-import User from './components/pages/User';
+// import User from './components/pages/User';
 import Header from './components/Header';
 // import SignupForm from './components/SignupForm';
-// import Posts from './components/Posts';
+import Home from './components/pages/Home';
+import LoginSignUp from './components/pages/LoginSignup';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <ChakraProvider theme={theme}>
       {/* <Box textAlign="center" fontSize="xl"> */}
         <Grid minH="100vh">
             < Header />
             {/* <ColorModeSwitcher justifySelf="flex-end" /> */}
-            {/* <LoginForm /> */}
-            {/* <SignupForm /> */}
-            {/* <Posts/> */}
-            < User />
+            <Route exact path="/" >
+              <Home />
+            </Route>
+            <Route exact path="/login/" >
+              <LoginSignUp />
+            </Route>
         </Grid>
       {/* </Box> */}
     </ChakraProvider>
+    </Router>
   );
 }
 
