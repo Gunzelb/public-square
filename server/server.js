1635763532e2c0fe6720aab2e90db101a48ffb17
Gunzelb <brandongunzel@gmail.com>
2021-10-19 18:53:06 -0700

    added comment functionality to schema

--- server/server.js
+++ server/server.js
@@ -1,36 +1,42 @@
 const express = require("express");
 const path = require("path");
-const { ApolloServer } = require("apollo-server-express");
-
 const db = require("./config/connection");
-const { typeDefs, resolvers } = require("./schemas");
+const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
+const { typeDefs, resolvers } = require("./schemas");
 
-const app = express();
 const PORT = process.env.PORT || 3001;
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-});
+async function startApollo(typeDefs, resolvers) {
+  const app = express();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
 
-server.applyMiddleware({ app });
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+  }
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`);
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  });
 }
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
-
-db.once("open", () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
-});
+startApollo(typeDefs, resolvers);
