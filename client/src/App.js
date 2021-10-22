442854dfd5194721c288d0f0a8ba2c4fe0e11d1d
Gunzelb <brandongunzel@gmail.com>
2021-10-21 19:31:04 -0700

    updated login and signup forms. made major changes to User.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,13 @@
 //Library imports
 import React from 'react';
-import { ChakraProvider, theme, Grid} from '@chakra-ui/react'; //VStack, Box, Grid,
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { ChakraProvider, theme, Grid } from '@chakra-ui/react'; //VStack, Box, Grid,
 
 // Components
 // import LoginForm from './components/LoginForm';
@@ -11,24 +18,50 @@ import Home from './components/pages/Home';
 import LoginSignUp from './components/pages/LoginSignup';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <Router>
-    <ChakraProvider theme={theme}>
-      {/* <Box textAlign="center" fontSize="xl"> */}
-        <Grid minH="100vh">
-            < Header />
+    <ApolloProvider client={client}>
+      <Router>
+        <ChakraProvider theme={theme}>
+          {/* <Box textAlign="center" fontSize="xl"> */}
+          <Grid minH="100vh">
+            <Header />
             {/* <ColorModeSwitcher justifySelf="flex-end" /> */}
-            <Route exact path="/" >
+            <Route exact path="/">
               <Home />
             </Route>
-            <Route exact path="/login/" >
+            <Route exact path="/login/">
               <LoginSignUp />
             </Route>
-        </Grid>
-      {/* </Box> */}
-    </ChakraProvider>
-    </Router>
+          </Grid>
+          {/* </Box> */}
+        </ChakraProvider>
+      </Router>
+    </ApolloProvider>
   );
 }
 
--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,131 +1,134 @@
 //Imports
-import React from "react"
-import { useState, useEffect } from "react"
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 //Chakra Components
-import { Flex, Input, Heading, Button } from "@chakra-ui/react";
-import { FormControl, FormLabel } from "@chakra-ui/react";
-import { useToast } from "@chakra-ui/react";
+import { Flex, Input, Heading, Button } from '@chakra-ui/react';
+import { FormControl, FormLabel } from '@chakra-ui/react';
+import { useToast } from '@chakra-ui/react';
 
 //Helper functions
 import { checkPassword, validateEmail } from '../utils/helpers';
 
 function LoginForm() {
+  //Input states
+  const [formState, setFormState] = useState({ email: '', password: '' });
 
-    //Input states
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  //Toast variables/states
+  const toast = useToast();
+  const [status, setStatus] = useState(false);
+  const [title, setTitle] = useState('It Works!');
+  const [description, setDescription] = useState('Omg it really did it');
+  const [toastType, setToastType] = useState('error');
 
-    //Toast variables/states
-    const toast = useToast();
-    const [status, setStatus] = useState(false);
-    const [title, setTitle] = useState("It Works!");
-    const [description, setDescription] = useState("Omg it really did it");
-    const [toastType, setToastType] = useState('error');
+  const [loginUser, { error }] = useMutation(LOGIN_USER);
 
-    useEffect(() => {
-        if (status) {
-            toast({
-                title: title,
-                description: description,
-                status: toastType,
-                duration: 3000,
-                isClosable: true
-            });
-            setStatus(false)
-        }
-    }, [status, toast, title, description, toastType]);
+  useEffect(() => {
+    if (status) {
+      toast({
+        title: title,
+        description: description,
+        status: toastType,
+        duration: 3000,
+        isClosable: true,
+      });
+      setStatus(false);
+    }
+  }, [status, toast, title, description, toastType]);
 
-    const handleInputChange = (e) => {
-        e.preventDefault()
+  const handleInputChange = e => {
+    // Getting the value and name of the input which triggered the change
+    const { name, value } = e.target;
+    // Set the state of the form to an object with the email and password as properties
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
 
-        // Getting the value and name of the input which triggered the change
-        const inputType = e.target.name;
-        const inputValue = e.target.value;
+  const handleSubmit = async e => {
+    e.preventDefault();
+    console.log(formState);
 
-        // Based on the input type, we set the state of either email, username, and password
-        if (inputType === 'email') {
-            setEmail(inputValue);
-        } else {
-            setPassword(inputValue);
-        }
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-
-        const login = {
-            email: email,
-            password: password
-        }
-        //Check if email is in the right format
-        if (!validateEmail(email) || !password) {
-            setTitle("Error");
-            setDescription('Email or password is invalid');
-            setToastType('error')
-            setStatus(true)
-            // We want to exit out of this code block if something is wrong so that the user can correct it
-            return;
-            // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
-          }
-
-        if(!checkPassword(password)){
-            setTitle("Error");
-            setDescription('Password between 8-14 characters');
-            setToastType('error')
-            setStatus(true)
-            return
-        }
-
-        else{
-            setTitle("");
-            setDescription('Login successfully ');
-            setToastType('success')
-            setStatus(true)
-        }
-        console.log(login);
+    //Check if email is in the right format
+    if (!validateEmail(formState.email) || !formState.password) {
+      setTitle('Error');
+      setDescription('Email or password is invalid');
+      setToastType('error');
+      setStatus(true);
+      // We want to exit out of this code block if something is wrong so that the user can correct it
+      return;
+      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
     }
 
+    if (!checkPassword(formState.password)) {
+      setTitle('Error');
+      setDescription('Password between 8-14 characters');
+      setToastType('error');
+      setStatus(true);
+      return;
+    } else {
+      try {
+        const { data } = await loginUser({
+          variables: { ...formState },
+        });
 
-    return (
-        <Flex direction="column" border="1px" p={8} me={3} rounded={6} width="30%">
-            <Heading mb={6} color="cyan.600">Log in</Heading>
-            <FormControl id="email" isRequired>
+        Auth.login(data.loginUser.token);
+      } catch (err) {
+        console.log(error);
+        console.log(err);
+        setTitle('Error');
+        setDescription('An error occurred!');
+        setToastType('error');
+        setStatus(true);
+        return;
+      }
+      setTitle('');
+      setDescription('Login successfully ');
+      setToastType('success');
+      setStatus(true);
+    }
+  };
 
-                <FormLabel>Email address</FormLabel>
-                <Input
-                    type="email"
-                    name="email"
-                    placeholder="jane_doe"
-                    variant="filled"
-                    mb={3}
-                    value={email}
-                    onChange={handleInputChange}
-                />
+  return (
+    <Flex direction="column" border="1px" p={8} me={3} rounded={6} width="30%">
+      <Heading mb={6} color="cyan.600">
+        Log in
+      </Heading>
+      <FormControl id="email" isRequired>
+        <FormLabel>Email address</FormLabel>
+        <Input
+          type="email"
+          name="email"
+          placeholder="jane_doe"
+          variant="filled"
+          mb={3}
+          value={formState.email}
+          onChange={handleInputChange}
+        />
+      </FormControl>
 
-            </FormControl>
+      <FormControl id="password" isRequired>
+        <FormLabel>Password</FormLabel>
+        <Input
+          type="password"
+          name="password"
+          placeholder="********"
+          variant="filled"
+          mb={3}
+          value={formState.password}
+          onChange={handleInputChange}
+        />
+      </FormControl>
 
-            <FormControl id="password" isRequired>
-
-                <FormLabel>Password</FormLabel>
-                <Input
-                    type="password"
-                    name="password"
-                    placeholder="********"
-                    variant="filled"
-                    mb={3}
-                    value={password}
-                    onChange={handleInputChange}
-                />
-
-            </FormControl>
-
-            <Button colorScheme="cyan" variant="outline" onClick={handleSubmit}>
-                Submit
-            </Button>
-
-        </Flex>
-    )
-};
+      <Button colorScheme="cyan" variant="outline" onClick={handleSubmit}>
+        Submit
+      </Button>
+    </Flex>
+  );
+}
 
 export default LoginForm;
