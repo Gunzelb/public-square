ca101f895a2e72eae3e24b884babf45f1a325b87
Gunzelb <brandongunzel@gmail.com>
2021-10-21 21:43:32 -0700

    created home page to render posts

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,13 +1,19 @@
-import React from "react"
+import React from 'react';
+import { useQuery } from '@apollo/client';
 
+import PostList from '../PostList';
+
+import { QUERY_POSTS } from '../../utils/queries';
 
 const Home = () => {
+  const { loading, data } = useQuery(QUERY_POSTS);
+  const posts = data?.posts || [];
 
-    return (
-        
-    )
-}
-
-
+  return (
+    <main>
+      <div>{loading ? <div>Loading...</div> : <PostList posts={posts} />}</div>
+    </main>
+  );
+};
 
 export default Home;
